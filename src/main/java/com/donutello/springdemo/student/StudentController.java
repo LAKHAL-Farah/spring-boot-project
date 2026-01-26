cc0721e20aa713ad8860a678fbb91c89677a4492
Lkhl-farah <farah.lakhal@esprit.tn>
2026-01-26 21:03:03 +0100

    first commit

---
+++ src/main/java/com/donutello/springdemo/student/StudentController.java
@@ -0,0 +1,16 @@
+package com.donutello.springdemo.student;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    public List<String> findAllStudents(){
+        return List.of(
+        );
+    }
+}
---
+++ src/test/java/com/donutello/springdemo/SpringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donutello.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
